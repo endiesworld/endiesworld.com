dcde9b17c2892bf5bef53e46d74f77444e40d834
endiesworld <endiesworld@gmail.com>
2021-02-17 04:01:57 +0100

    added a build dir

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import {useState} from "react" ;
+import {dommyData} from "./domyData" ;
+import {Table, Button} from "reactstrap" ;
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome" ;
+import {faImages , faThumbsUp , faThumbsDown, faMoneyCheckAlt, faSearchDollar} from "@fortawesome/free-solid-svg-icons" ;
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ const [isLoading, setIsLoading] = useState(false) ;
+ const [invoices, setInvoice] = useState(dommyData) ;
+
+ function remove (id){
+   var updatedInvoice = invoices.filter(invoice => invoice.id !== id) ;
+   setInvoice(updatedInvoice) ;
+ }
+ 
+ const invoiceComponent = invoices.map(invoice =>
+  <tr key = {invoice.id}>
+    {console.log(invoice.id)}
+    <td>{invoice.Vendor}</td>
+    <td>{invoice.Amount}</td>
+    <td>{invoice.Invoice}</td>
+    <td>{invoice.Date}</td>
+    <td><Button className ="btn btn-lg btn-success" onClick={() => remove(invoice.id)} > <FontAwesomeIcon icon = {faThumbsUp} /> OK </Button></td>
+    <td><Button className ="btn btn-lg btn-danger" onClick={() => remove(invoice.id)} > <FontAwesomeIcon icon = {faThumbsDown} /> Not OK </Button></td>
+    <td><Button className ="btn btn-lg btn-info" onClick={() => remove(invoice.id)} > <FontAwesomeIcon icon = {faMoneyCheckAlt} /> 50% </Button></td>
+    <td><Button className ="btn btn-lg btn-warning" onClick={() => remove(invoice.id)} > <FontAwesomeIcon icon = {faSearchDollar} />?? </Button></td>
+    <td><Button className ="btn btn-lg btn-info" onClick={() => remove(invoice.id)} > <FontAwesomeIcon icon = {faImages} /> Image </Button></td>
+ </tr>)
+
+ return (isLoading) ? 
+  (<div> Loading...</div>) :
+   (
+    <div className="container border border-secondary rounded center">
+      <div className ="row">
+        <div className ="col-12">
+          <h4> Pending Invoices - The test Company</h4>
+        </div>
+      </div>
+      <div className ="row">
+        <div className = ".col-xs-12 center text-center">
+          <Table dark responsive striped bordered hover>
+            <thead>
+              <tr>
+                <th>Vendor</th>
+                <th>Amount</th>
+                <th>Invoice No.</th>
+                <th>Date</th>
+                <th colSpan ="4">Actions</th>
+                <th>Image</th>
+              </tr>
+            </thead>
+            <tbody>
+              {invoices.length === 0 ? <tr colSpan = "9">
+                <td colSpan = "9">All caught up and deleted! </td>
+                
+              </tr> : invoiceComponent
+              }
+            </tbody>
+          </Table>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/domyData.js
@@ -0,0 +1,23 @@
+ export const dommyData = [
+    {
+    "id": "100",
+    "Vendor": "Hankook",
+    "Amount": "$18,000",
+    "Invoice": "1324" ,
+    "Date": "13/02/2021"
+    },
+    {
+    "id": "200",
+    "Vendor": "Hankook",
+    "Amount": "$18,000",
+    "Invoice": "1324" ,
+    "Date": "13/02/2021"
+    },
+    {
+    "id": "300",
+    "Vendor": "Hankook",
+    "Amount": "$18,000",
+    "Invoice": "1324" ,
+    "Date": "13/02/2021"
+    },
+]
